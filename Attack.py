f87bb35a462b46d2d3229c421d17a0ecb3e232c2
ZaidGuy <116162689+ZaidGuy@users.noreply.github.com>
2022-12-08 17:15:39 +0300

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -5,7 +5,7 @@ import threading
 from discord.ext import commands
 import os
 import random
-token = "MTA0ODEyODI1ODk0MTY2NTM3MQ.GVQz0j.SyKfqBXhN2f0CxBuRkcDdWIB39VSmXII9ByzjI"
+token = "MTA0ODEyODI1ODk0MTY2NTM3MQ.GlX5Zb.zow7O8x5YBQxDBKFV0-3xQ2eTvQaDu_7SYgr_I"
 client = commands.Bot(command_prefix='xd!',intents=discord.Intents.all()) 
 client.remove_command('help')
 @client.command()
